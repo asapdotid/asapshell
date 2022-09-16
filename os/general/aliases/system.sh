9bc2c240ca4de3cb8cfe2c829ff353a617038809
Asapdotid <asapdotid@gmail.com>
2022-09-16 10:55:42 +0700

    🐛 FIX: check contion string not empty

--- os/general/aliases/system.sh
+++ os/general/aliases/system.sh
@@ -21,7 +21,7 @@ alias lt='du -sh * | sort -h'
 
 #Change directory
 function cd_home_dot() {
-  if [ -z "$1" && -d "$HOME/.$1" ]; then
+  if [ ! -z "$1" && -d "$HOME/.$1" ]; then
     cd "$HOME/.$1"
   else
     error "Please check your directory destination first!"
