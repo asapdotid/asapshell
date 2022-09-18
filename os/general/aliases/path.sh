e262679e9bdfdee524e5c3b3b0d3f03616d486aa
Asapdotid <asapdotid@gmail.com>
2022-09-19 01:22:59 +0700

    🐛 FIX: path aliases

--- os/general/aliases/path.sh
+++ os/general/aliases/path.sh
@@ -16,39 +16,33 @@ alias home:doc='home_path Documents'
 
 #Change home directory
 function home_path() {
-  if [[ -z "$1" ]]; then
+  if [ -z "$1" ]; then
     cd $HOME && exa -labGF --header --git --icons $HOME
+  elif [[ ! -z "$1" && -d "$HOME/$1" ]]; then
+    cd "$HOME/$1" && exa -labGF --header --git --icons "$HOME/$1"
   else
-    if [[ -d "$HOME/$1" ]]; then
-      cd "$HOME/$1" && exa -labGF --header --git --icons "$HOME/$1"
-    else
-      error "Directory doest not exist in the Home"
-    fi
+    error "Directory doest not exist in the Home"
   fi
 }
 
 #Change to Labs directory
 function lab_path() {
-  if [[ -z "$1" ]]; then
+  if [ -z "$1" ]; then
     cd $LAB_LOCATION && exa -labGF --header --git --icons $LAB_LOCATION
+  elif [[ ! -z "$1" && -d "$LAB_LOCATION/$1" ]]; then
+    cd "$LAB_LOCATION/$1" && exa -labGF --header --git --icons "$LAB_LOCATION/$1"
   else
-    if [[ -d "$LAB_LOCATION/$1" ]]; then
-      cd "$LAB_LOCATION/$1" && exa -labGF --header --git --icons "$LAB_LOCATION/$1"
-    else
-      error "Directory doest not exist in the Labs"
-    fi
+    error "Directory doest not exist in the Labs"
   fi
 }
 
 #Change to Projects directory
 function project_path() {
-  if [[ -z "$1" ]]; then
+  if [ -z "$1" ]; then
     cd $PROJECT_LOCATION && exa -labGF --header --git --icons $PROJECT_LOCATION
+  elif [[ ! -z "$1" && -d "$PROJECT_LOCATION/$1" ]]; then
+    cd "$PROJECT_LOCATION/$1" && exa -labGF --header --git --icons "$PROJECT_LOCATION/$1"
   else
-    if [[ -d "$PROJECT_LOCATION/$1" ]]; then
-      cd "$PROJECT_LOCATION/$1" && exa -labGF --header --git --icons "$PROJECT_LOCATION/$1"
-    else
-      error "Directory doest not exist in the Projects"
-    fi
+    error "Directory doest not exist in the Projects"
   fi
 }
