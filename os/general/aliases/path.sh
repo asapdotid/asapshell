57710ecac6e61e2dbb4acf47e26c38574e956e1a
Asapdotid <asapdotid@gmail.com>
2022-09-18 23:13:41 +0700

    👌 IMPROVE: update aliases for path

--- os/general/aliases/path.sh
+++ os/general/aliases/path.sh
@@ -16,10 +16,10 @@ alias home:doc='home_path Documents'
 
 #Change home directory
 function home_path() {
-  if [ -z "$1" ]; then
+  if [[ -z "$1" ]]; then
     cd $HOME && exa -labGF --header --git --icons $HOME
   else
-    if [ -d "$HOME/$1" ]; then
+    if [[ -d "$HOME/$1" ]]; then
       cd "$HOME/$1" && exa -labGF --header --git --icons "$HOME/$1"
     else
       error "Directory doest not exist in the Home"
@@ -29,10 +29,10 @@ function home_path() {
 
 #Change to Labs directory
 function lab_path() {
-  if [ -z "$1" ]; then
+  if [[ -z "$1" ]]; then
     cd $LAB_LOCATION && exa -labGF --header --git --icons $LAB_LOCATION
   else
-    if [ -d "$LAB_LOCATION/$1" ]; then
+    if [[ -d "$LAB_LOCATION/$1" ]]; then
       cd "$LAB_LOCATION/$1" && exa -labGF --header --git --icons "$LAB_LOCATION/$1"
     else
       error "Directory doest not exist in the Labs"
@@ -42,10 +42,10 @@ function lab_path() {
 
 #Change to Projects directory
 function project_path() {
-  if [ -z "$1" ]; then
+  if [[ -z "$1" ]]; then
     cd $PROJECT_LOCATION && exa -labGF --header --git --icons $PROJECT_LOCATION
   else
-    if [ -d "$LAB_LOCATION/$1" ]; then
+    if [[ -d "$LAB_LOCATION/$1" ]]; then
       cd "$PROJECT_LOCATION/$1" && exa -labGF --header --git --icons "$PROJECT_LOCATION/$1"
     else
       error "Directory doest not exist in the Projects"
