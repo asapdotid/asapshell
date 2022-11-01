b1fb75524e2ba19a7efb69a5f1bb3ad71ac4b4f7
Asapdotid <asapdotid@gmail.com>
2022-11-01 15:29:30 +0700

    revamp composer aliases

--- os/general/aliases/composer.sh
+++ os/general/aliases/composer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # PHP Composer
 alias com='composer'
-alias com:v='composer -V'
+alias com:-v='composer -V'
 alias com:init='composer init'
 alias com:i='composer install'
 alias com:up='composer update'
-alias com:g:up='composer global update'
 alias com:ug='composer upgrade'
 alias com:re='composer require'
 alias com:re:d='composer require --dev'
-alias com:g:re='composer global require'
 alias com:rm='composer remove'
 alias com:rm:d='composer remove --dev'
+alias com:g:re='composer global require'
+alias com:g:up='composer global update'
 alias com:g:s='composer global show'
