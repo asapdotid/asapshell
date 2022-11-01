e508e6246a44ce3bbf3f2b4b31e152bc61f19f8d
Asapdotid <asapdotid@gmail.com>
2022-11-01 15:33:00 +0700

    update nvm aliases

--- os/general/aliases/nvm.sh
+++ os/general/aliases/nvm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # NVM
-alias nvm:v='nvm --version'
+alias nvm:-v='nvm --version'
 alias nvm:l='nvm ls'
 alias nvm:l:r='nvm ls-remote --lts'
 alias nvm:v:r="nvm version-remote --lts"
 alias nvm:i='nvm install'
 alias nvm:i:lts="nvm install '\''lts/*'\'' --reintsall-packages-from=default --latest-npm"
-alias nvm:u='nvm uninstall'
-alias nvm:s='nvm use'
-alias nvm:d='nvm alias default'
+alias nvm:ui='nvm uninstall'
+alias nvm:u='nvm use'
+alias nvm:a:d='nvm alias default'
