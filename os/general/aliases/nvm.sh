2017a3dd58399374434e6ab44e0ecebd85801467
Asapdotid <asapdotid@gmail.com>
2022-11-11 14:23:57 +0700

    revamp remote list and reinstall package

--- os/general/aliases/nvm.sh
+++ os/general/aliases/nvm.sh
@@ -2,10 +2,22 @@
 # NVM
 alias nvm:-v='nvm --version'
 alias nvm:l='nvm ls'
-alias nvm:l:r='nvm ls-remote --lts'
-alias nvm:v:r="nvm version-remote --lts"
+alias nvm:r:l='nvm ls-remote --lts'
+alias nvm:r:ls='nvm ls-remote --lts | grep Latest'
+alias nvm:r:v="nvm version-remote --lts"
 alias nvm:i='nvm install'
-alias nvm:i:lts="nvm install '\''lts/*'\'' --reintsall-packages-from=default --latest-npm"
 alias nvm:ui='nvm uninstall'
+alias nvm:r='nvm_reinstall'
 alias nvm:u='nvm use'
 alias nvm:a:d='nvm alias default'
+
+# Function
+function nvm_reinstall() {
+  local to_version=${1:=lts}
+  local from_version=${2:=default}
+  if [[ "$to_version" -eq 'lts' && "$from_version" -eq 'default' ]]; then
+    nvm install lts/* --reintsall-packages-from=default --latest-npm
+  else
+    nvm install $to_version --reintsall-packages-from=$from_version --latest-npm
+  fi
+}
