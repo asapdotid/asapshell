fcf26a4229d1ba21b69f3a114dcfbd2bc37a19dc
Asapdotid <asapdotid@gmail.com>
2022-12-20 10:33:36 +0700

    👌 IMPROVE: add alias help

--- os/general/aliases/custom.sh
+++ os/general/aliases/custom.sh
@@ -13,6 +13,9 @@ alias code="$VSCODE"
 # Noevim
 alias nv='nvim'
 
+# help aliases
+alias -?='--help'
+
 # Get IP
 alias ip:p='get_public_ip'
 alias ip:l='get_local_ip'
@@ -24,21 +27,21 @@ alias myhost='bat /etc/hosts'
 # Functions
 # Using DIG Utility (Arch linux pacman -Ss bind)
 function get_public_ip() {
-    local myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-    if [[ -z "$myip" ]]; then
-        public_ip=$(curl --silent icanhazip.com)
-    else
-        public_ip=$myip
-    fi
-    info "My WAN/Public IP address: ${YELLOW}${public_ip}${RESET}"
+  local myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+  if [[ -z "$myip" ]]; then
+    public_ip=$(curl --silent icanhazip.com)
+  else
+    public_ip=$myip
+  fi
+  info "My WAN/Public IP address: ${YELLOW}${public_ip}${RESET}"
 }
 
 function get_local_ip() {
-    local device=$1
-    if [[ -z "$device" ]]; then
-        local_ip="ip addr"
-    else
-        local_ip="ip addr show dev $device"
-    fi
-    bash -c $local_ip
+  local device=$1
+  if [[ -z "$device" ]]; then
+    local_ip="ip addr"
+  else
+    local_ip="ip addr show dev $device"
+  fi
+  bash -c $local_ip
 }
