121f2d95435cfcba88ff55b181090581478f758c
Asapdotid <asapdotid@gmail.com>
2023-05-03 16:45:52 +0700

    ✨ NEW FEATURE: add expressvpn aliases and update git merge

--- os/linux/index.sh
+++ os/linux/index.sh
@@ -3,7 +3,8 @@
 __LSYSTEM="$ASAPSHELL_DIR/os/linux/aliases/system.sh"
 __LPACKAGE="$ASAPSHELL_DIR/os/linux/aliases/package.sh"
 __LSERVICE="$ASAPSHELL_DIR/os/linux/aliases/service.sh"
-__LINUXFILES=($__LSYSTEM $__LPACKAGE $__LSERVICE)
+__LEVPN="$ASAPSHELL_DIR/os/linux/aliases/expressvpn.sh"
+__LINUXFILES=($__LSYSTEM $__LPACKAGE $__LSERVICE $__LEVPN)
 
 for i in "${__LINUXFILES[@]}"; do
   if [ -f "$i" ]; then
