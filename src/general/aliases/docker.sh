a064d4faba76d3778fdf609097fd678bf14fca77
Asapdotid <asapdotid@gmail.com>
2023-10-13 10:39:10 +0700

    ⚡️ IMPROVE: update docker aliases

--- src/general/aliases/docker.sh
+++ src/general/aliases/docker.sh
@@ -24,9 +24,51 @@ alias dc:rm:i='docker rmi -f'
 alias dc:clean='docker_cleanup'
 alias dc:prune='docker system prune -a --volumes -f'
 alias dc:in='docker_in_docker'
+
 # Docker network
-alias dc:net='docker network'
 alias dc:net:-h='docker network --help'
+alias dc:net='docker network'
+alias dc:net:l='docker network ls'
+alias dc:net:c='docker network create'
+alias dc:net:con='docker network connetc'
+alias dc:net:dis='docker network disconnect'
+alias dc:net:is='docker network inspect'
+alias dc:net:p='docker network prune'
+alias dc:net:l='docker network ls'
+alias dc:net:r='docker network rm'
+
+# Docker node
+alias dc:node='docker node'
+alias dc:node:l='docker node ls'
+alias dc:node:r='docker node rm'
+alias dc:node:p='docker node prune'
+alias dc:node:u='docker node update'
+alias dc:node:is='docker node inspect'
+alias dc:node:ps='docker node ps'
+alias dc:node:pr='docker node promote'
+alias dc:node:dm='docker node demode'
+
+# Docker service
+alias dc:svc='docker service'
+alias dc:svc:-h='docker service --help'
+alias dc:svc:l='docker service ls'
+alias dc:svc:r='docker service rm'
+alias dc:svc:p='docker service prune'
+alias dc:svc:is='docker service inspect'
+alias dc:svc:ps='docker service ps'
+alias dc:svc:c='docker service create'
+alias dc:svc:rb='docker service rollback'
+alias dc:svc:s='docker service scale'
+alias dc:svc:u='docker service update'
+
+# Docker secret
+alias dc:sec='docker secret'
+alias dc:sec:-h='docker secret --help'
+alias dc:sec:l='docker secret ls'
+alias dc:sec:r='docker secret rm'
+alias dc:sec:c='docker secret create'
+alias dc:sec:is='docker secret inspect'
+
 # Docker image
 alias dc:img='docker image'
 alias dc:img:-h='docker image --help'
@@ -36,6 +78,7 @@ alias dc:img:push='docker image push'
 alias dc:img:rm='docker image rm -f'
 alias dc:img:inspect='docker image inspect -f'
 alias dc:img:prune='docker image prune -f'
+
 # Docker container
 alias dc:con='docker container'
 alias dc:con:-h='docker container --help'
@@ -45,8 +88,7 @@ alias dc:con:logs='docker container logs'
 alias dc:con:exec='docker container exec'
 alias dc:con:top='docker container top'
 alias dc:con:prune='docker container prune -f'
-# Docker swarm
-alias dc:swarm='docker swarm'
+
 # Docker Compose 2.x
 alias dc:com='docker compose'
 alias dc:com:-h='docker compose --help'
@@ -60,14 +102,18 @@ alias dc:com:run='docker compose run'
 alias dc:com:pause='docker compose pause'
 alias dc:com:top='docker compose top'
 alias dc:com:exec='docker compose exec'
-# Docker Compose 1.x
-alias dc-com='docker-compose'
-alias dc-com:-h='docker-compose --help'
-alias dc-com:-v='docker-compose --version'
-alias dc-com:u='docker-compose up -d'
-alias dc-com:d='docker-compose down --remove-orphans -v'
-alias dc-com:build='docker-compose up -d --build -f'
-alias dc-com:logs='docker-compose logs'
+
+# Docker swarm
+alias dc:sw='docker swarm'
+alias dc:sw:-h='docker swarm --help'
+alias dc:sw:i='docker swarm init'
+alias dc:sw:ca='docker swarm ca'
+alias dc:sw:u='docker swarm update'
+alias dc:sw:l='docker swarm leave'
+alias dc:sw:j='docker swarm join'
+alias dc:sw:jt='docker swarm join-token'
+alias dc:sw:ul='docker swarm unlock'
+alias dc:sw:ulk='docker swarm unlock-key'
 
 # Functions
 # Docker CLeanup
@@ -98,4 +144,12 @@ docker_in_docker() {
   fi
   ex=$(docker ps --filter name="$filter" -q | head -1)
   ${prefix} docker exec -it "$user" "$ex" "$shell"
+}
+
+docker_swarm_init() {
+  local advertise_addr
+  local listen_addr
+  advertise_addr=${1:-0.0.0.0}
+  listen_addr=${2:-0.0.0.0}
+  docker swarm init --advertise-addr "$advertise_addr" --listen-addr "$listen_addr"
 }
